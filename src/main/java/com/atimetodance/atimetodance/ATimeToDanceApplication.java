6839b46bae5e0f254847b5e09ac6fd3b45d15e0d
Luke Van Delden <vandelden.luke@gmail.com>
2023-07-15 15:31:26 -0500

    creating project

---
+++ src/main/java/com/atimetodance/atimetodance/ATimeToDanceApplication.java
@@ -0,0 +1,13 @@
+package com.atimetodance.atimetodance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATimeToDanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ATimeToDanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atimetodance/atimetodance/ATimeToDanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atimetodance.atimetodance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATimeToDanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
